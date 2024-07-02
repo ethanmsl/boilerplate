b2988b6ea40facab0bc033ff887948404539a0c5
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2024-07-02 09:45:54 -0700

    Feat: tracing subscriber options

--- Rust-Boilerplate/libmain.rs
+++ Rust-Boilerplate/libmain.rs
@@ -20,7 +20,34 @@ pub mod boilerplate {
     /// # Note:
     /// - Not optimal.  But has more info than default and allows easily setting a default log level.
     /// - We should return a `Result`, but I don't want to mess with the flow of this repo's tour by adding different error types and consequent handling strategies.
-    pub fn tracing_subscribe_boilerplate(kind: SubKind) {
+    pub mod tracing {
+    use tracing_subscriber::EnvFilter;
+
+    /// Start boilerplate tracing subscriber, with a default log level, when none is specified
+    ///
+    /// Allows easily setting regular tracing or console input via config.  
+    /// Does its job alright for now.
+    ///
+    /// # Note:
+    /// - Not optimal.  But has more info than default and allows easily setting a default log level.
+    /// - We should return a `Result`, but I don't want to mess with the flow of this repo's tour by adding different error types and consequent handling strategies.
+    pub fn tracing_subscribe_boilerplate(default_env: impl Into<String>) {
+        let filter = EnvFilter::try_new(
+                    std::env::var("RUST_LOG").unwrap_or_else(|_| default_env.into()),
+                )
+                .expect("Valid filter input provided.");
+
+        tracing_subscriber::fmt().pretty()
+                                 .with_env_filter(filter)
+                                 .with_file(true)
+                                 .with_line_number(true)
+                                 .with_thread_ids(true)
+                                 .with_target(true)
+                                 .init();
+    }
+
+
+    pub fn alt_tracing_subscribe_boilerplate(kind: SubKind) {
         // region:    --- tracing_subscriber
 
         match kind {
